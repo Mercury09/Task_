927953b680f896904f76e6aec9014c1e0651c62e
m.alibi143@gmail.com <m.alibi143@gmail.com>
2021-05-08 14:58:26 +0600

    WEEK2

---
+++ week2/task1.py
@@ -0,0 +1,76 @@
+
+# №1------------------------------------------------------------------------------------
+
+
+# a = input("Please write the words: ")
+# a = sorted(a)
+# s = ''.join(a)
+# print(s)
+
+
+# №2------------------------------------------------------------------------------------
+
+
+
+# a = int(input("Guess one number of 1-50: "))
+# array = []
+# count = 0
+# i = 1
+# x = 1
+
+# for i in range(i,51):
+# 	array.append(i)
+# print(array)
+
+# for x in range(x,4):
+# 	b = int(input("Try to find guessed the number: "))	
+# 	if b == a:
+# 		print("Nice! succeeded the guess number")
+# 		break
+# 	elif b > a:
+# 		print("More")
+# 	elif b < a:
+# 		print("Less")
+# 	if x == 3:
+# 		print("GAME OVER")
+# 		break
+	
+
+
+# №3------------------------------------------------------------------------------------
+
+
+
+# import random
+
+# ran = int(random.randrange(100, 1000))
+# a = int(ran / 100)
+# b = int((ran / 10) % 10)
+# c = int(ran % 10)
+
+# x = a + b + c
+# y = a * b * c
+# print(ran, x , y)
+
+
+
+# №4------------------------------------------------------------------------------------
+
+
+# import requests
+
+# url = 'https://webhook.site/a748916a-2d47-4735-82b4-bd570c36732b'
+# s = {'xyz': '987'}
+
+# x = requests.post(url, data = s)
+
+
+
+
+# №5------------------------------------------------------------------------------------
+
+
+import requests
+
+s = {"key": "value"}
+x = requests.get('https://webhook.site/a748916a-2d47-4735-82b4-bd570c36732b', data = s)
